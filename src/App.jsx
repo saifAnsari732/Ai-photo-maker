83230811beac588ab75a2274c0e7d4759f2f66f4
Saifuddin Ansari <ansarisaifuddin732@gmail.com>
2026-07-21 17:26:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
-import LoginPage from './pages/LoginPage';
+import LoginPae from './pages/LoginPag';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import AdminPage from './pages/AdminPage';
