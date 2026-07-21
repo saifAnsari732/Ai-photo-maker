67b02861961b4f3b7099eb7384a21a724dc5a70b
Saifuddin Ansari <ansarisaifuddin732@gmail.com>
2026-07-21 17:01:55 +0530

    Fix proxy target URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://ai-photo-maker-back.onrender.com',
+        target: 'https://ai-photo-maker-back.onrender.co',
         // target: 'http://localhost:5000',
         changeOrigin: true,
       }
